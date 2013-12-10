100c9a527f9291e0ddc10fb50765ecc64747e55d
Trevor Muñoz <trevor.munoz@gmail.com>
2013-12-10 08:51:03 -0500

    More config tweaking

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 CSRF_ENABLED = True
 SECRET_KEY = os.environ['SECRET_KEY']
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-AWS_URI = 'http://54.221.196.204'
+AWS_URI = os.environ['AWS_URI']
